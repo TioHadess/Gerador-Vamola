87dfb1770dd9bdacc7b5d47ba426dd3ab9d6644e
C.Rocha <rochacaik@gmail.com>
2023-11-09 11:41:52 -0300

    perfil/editar conserto

--- index.py
+++ index.py
@@ -108,40 +108,35 @@ def perfil():
     if request.method == 'POST':
         req = request.form
 
-    # Lógica para gerar uma senha aleatória ao acessar a rota
-    tamanho_da_senha = 12  # Você pode ajustar o tamanho da senha conforme necessário
-    caracteres = string.ascii_letters + string.digits + string.punctuation
-    senha_segura = ''.join(choice(caracteres) for _ in range(tamanho_da_senha))
+        # Lógica para gerar uma senha aleatória ao enviar o formulário
+        tamanho_da_senha = 12
+        caracteres = string.ascii_letters + string.digits + string.punctuation
+        senha_segura = ''.join(choice(caracteres) for _ in range(tamanho_da_senha))
 
-    return render_template('perfil.html', nome_usuario=current_user.nome,email_usuario=current_user.email, senha_segura=senha_segura)
+    return render_template('perfil.html', nome_usuario=current_user.nome, email_usuario=current_user.email, senha_segura=senha_segura)
 
 @app.route('/perfil/editar', methods=['GET', 'POST'])
 @login_required
 def editar():
     if request.method == 'POST':
         req = request.form
-        to_edit_user = User.query.filter_by(id = current_user.id).first()
-        to_edit_user.nome = req['nome']
-        db.session.commit()
-        current_user.nome = req['nome']
-        return redirect('/perfil')
-    elif request.method == 'POST':
-        req = request.form
         to_edit_user = User.query.filter_by(id=current_user.id).first()
-        to_edit_user.email = req['email']
+
+        if 'nome' in req and req['nome']:
+            to_edit_user.nome = req['nome']
+            current_user.nome = req['nome']
+        if 'email' in req and req['email']:
+            to_edit_user.email = req['email']
+            current_user.email = req['email']
+        if 'senha' in req and req['senha']:
+            to_edit_user.senha = req['senha']
+            current_user.senha = req['senha']
+
         db.session.commit()
-        current_user.email = req['email']
-        return redirect('/perfil')
-    elif request.method == 'POST':
-        req = request.form
-        to_edit_user = User.query.filter_by(id=current_user.id).first()
-        to_edit_user.senha = req['senha']
-        db.session.commit()
-        current_user.senha = req['senha']
+
         return redirect('/perfil')
 
-    return render_template('editar.html', nome_usuario=current_user.nome)
-
+    return render_template('editar.html', nome_usuario=current_user.nome, email_usuario=current_user.email, senha_usuario=current_user.senha)
 
 @app.route('/perfil/sair', methods=['GET'])
 @login_required
